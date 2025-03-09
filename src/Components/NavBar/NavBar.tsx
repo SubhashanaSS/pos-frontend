116b1f2d5cd6b4946d642a511b4d87f4900f7adb
SubhashanaSS <subhashana201@gmail.com>
2025-03-09 23:05:27 +0530

    Added Header NavBar anf Footer

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,28 @@
+import './Navbar.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const navLogin = () => {
+    navigate('/login'); // Navigates to /login
+  };
+
+  return (
+    <header className="header">
+      <Link to="/" className="logo">TDM</Link>
+
+      <nav className="navbar">
+        <Link to="/dashboard" >Home</Link>
+        <Link to="/categories" >Category</Link>
+        <Link to="/item">Item</Link>
+        <Link to="/stock">Stock</Link>
+        <Link to="/cart">POS</Link>
+        {/* Use onClick only if you want to handle it differently; otherwise, Link is enough */}
+        <Link to="/" onClick={navLogin}>Logout</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
